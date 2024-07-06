46066171f7fab0a090e59d255e7426b1f41e3aca
Mayur Bapodra <eighthsamurai@hotmail.com>
2024-07-06 22:10:58 +0100

    Error page fixes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,8 +7,8 @@ export default function ErrorBoundary({ error }: { error: Error }) {
     <div className="h-screen w-full flex flex-col items-center justify-center text-center">
       <div className="text-8xl mb-4">Uh-oh!</div>
       <div className="text-lg mb-12 text-gray-600">
-        Sorry, it looks like something went wrong. We'll fix it as soon as we
-        can!
+        Sorry, it looks like something went wrong. We&apos;ll fix it as soon as
+        we can!
       </div>
       <div className="text-sm text-gray-700">{error.message}</div>
       <div className="mt-8">
