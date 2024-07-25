1be21a50f968ff7f108e66f5ceec7cef67c575ed
Mayur Bapodra <mayur.bapodra@carbon.ci>
2024-07-25 15:08:45 +0100

    Deletion working

--- src/_components/fullPageSpinner/FullPageSpinner.tsx
+++ src/_components/fullPageSpinner/FullPageSpinner.tsx
@@ -1,6 +1,6 @@
 export default function FullPageSpinner() {
   return (
-    <div className="fixed flex w-screen h-screen justify-center items-center overflow-hidden">
+    <div className="fixed top-0 left-0 flex w-screen h-screen justify-center items-center overflow-hidden">
       <div className="loading loading-bars loading-lg text-primary absolute" />
     </div>
   );
--- src/_components/playlistPanel/PlaylistPanel.tsx
+++ src/_components/playlistPanel/PlaylistPanel.tsx
@@ -1,19 +1,30 @@
 'use client';
 
+import useGetAuthToken from '@/_hooks/useGetAuthToken';
 import { clientSpotifyFetch } from '@/_utils/clientUtils';
 import { SpotifyPlaylist } from '@/types';
 import classNames from 'classnames';
 import Image from 'next/image';
-import { useCallback, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
+import FullPageSpinner from '../fullPageSpinner/FullPageSpinner';
 
 interface PlaylistPanelProps {
   playlist: SpotifyPlaylist;
 }
 
 export default function PlaylistPanel({ playlist }: PlaylistPanelProps) {
+  const params = useSearchParams();
   const [showDialog, setShowDialog] = useState(false);
+  const [updatedPlaylist, setUpdatedPlaylist] =
+    useState<SpotifyPlaylist>(playlist);
   const [showSecondDialog, setShowSecondDialog] = useState(false);
-  const;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [confirmationInput, setConfirmationInput] = useState<string>('');
+  const [showSuccessToast, setShowSuccessToast] = useState(
+    !!params.get('success'),
+  );
+  const authToken = useGetAuthToken();
 
   const handleFirstConfirmClicked = () => {
     setShowDialog(false);
@@ -21,24 +32,90 @@ export default function PlaylistPanel({ playlist }: PlaylistPanelProps) {
   };
 
   const handleClearConfirmed = useCallback(async () => {
-    const tracksResponse = await clientSpotifyFetch;
-  }, [playlist]);
+    setShowSecondDialog(false);
+    setLoading(true);
+
+    let trackUris: string[] = [];
+    let nextUrl = playlist.tracks.href;
+
+    try {
+      while (nextUrl) {
+        const tracksResponse = await fetch(nextUrl, {
+          headers: {
+            Authorization: authToken,
+          },
+        });
+
+        const data = await tracksResponse.json();
+
+        if (data.items.length) {
+          const newTrackUris = data.items.map(
+            (item: { track: { uri: string } }) => item.track.uri,
+          );
+          trackUris = [...trackUris, ...newTrackUris];
+        }
+
+        nextUrl = data.next;
+      }
+
+      const deleteCallsCount = Math.ceil(trackUris.length / 100);
+
+      const promises = Array.from(Array(deleteCallsCount).keys()).map((i) => {
+        const urisToDelete = trackUris.slice(i * 100, i * 100 + 100);
+
+        return clientSpotifyFetch(`playlists/${playlist.id}/tracks`, {
+          headers: {
+            Authorization: authToken,
+          },
+          body: JSON.stringify({
+            tracks: urisToDelete.map((uri) => ({
+              uri,
+            })),
+            snapshot_id: playlist.snapshot_id,
+          }),
+          method: 'DELETE',
+        });
+      });
+
+      await Promise.all(promises);
+      setShowSuccessToast(true);
+      setUpdatedPlaylist({
+        ...playlist,
+        tracks: {
+          ...playlist.tracks,
+          total: 0,
+        },
+      });
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.error(e);
+    }
+  }, [authToken, playlist]);
+
+  useEffect(() => {
+    if (showSuccessToast) {
+      setTimeout(() => {
+        setShowSuccessToast(false);
+      }, 4000);
+    }
+  }, [showSuccessToast]);
 
   return (
     <>
       <div className="bg-slate-800 mb-4 rounded-md w-full p-4 flex justify-between items-center">
         <div className="flex items-center">
           <Image
-            src={playlist.images[0].url}
-            alt={playlist.name}
+            src={updatedPlaylist.images[0].url}
+            alt={updatedPlaylist.name}
             width={0}
             height={0}
             style={{ width: '64px', height: 'auto' }}
           />
           <div className="ml-8">
-            {playlist.name}{' '}
+            {updatedPlaylist.name}{' '}
             <span className="text-slate-500">
-              ({playlist.tracks.total} tracks)
+              ({updatedPlaylist.tracks.total} tracks)
             </span>
           </div>
         </div>
@@ -74,19 +151,50 @@ export default function PlaylistPanel({ playlist }: PlaylistPanelProps) {
       >
         <div className="modal-box">
           <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Are you definitely sure??!!</p>
+          <p className="py-4">
+            Are you definitely sure??!! Type 1234 into the box below
+          </p>
+          <p className="py-4">
+            <input
+              type="text"
+              placeholder="Type 1234 here"
+              className="input input-bordered input-lg input-primary w-1/2 max-w-xs"
+              onChange={(e) => setConfirmationInput(e.target.value)}
+            />
+          </p>
           <div className="modal-action">
             <form method="dialog">
               <button className="btn" onClick={() => setShowDialog(false)}>
                 Cancel
               </button>
-              <button className="btn btn-error" onClick={handleClearConfirmed}>
+              <button
+                disabled={confirmationInput !== '1234'}
+                className="btn btn-error"
+                onClick={handleClearConfirmed}
+              >
                 Confirm
               </button>
             </form>
           </div>
         </div>
       </dialog>
+      {showSuccessToast && (
+        <button
+          className="toast toast-center"
+          onClick={() => setShowSuccessToast(false)}
+        >
+          <div className="alert alert-success">
+            <span>
+              {playlist.tracks.total} tracks deleted from {playlist.name}
+            </span>
+          </div>
+        </button>
+      )}
+      {loading && (
+        <div className="w-full h-full bg-slate-700 bg-opacity-85">
+          <FullPageSpinner />
+        </div>
+      )}
     </>
   );
 }
